eab501225fc52a7041510c4ead2654fddd1d19fa
lucasbastos <lucasbastos.ecomp@gmail.com>
2021-08-26 14:58:30 -0300

    feat: swagger config description

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('API - Professor')
+    .setDescription('Descrição da API do Professor')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('professor')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
